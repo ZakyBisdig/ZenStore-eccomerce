0f7dcc7d538fbdcf3ce1ba653f198c51717a1edb
Zaky Chahyadi <147672935+ZakyBisdig@users.noreply.github.com>
2024-04-15 08:47:25 +0800

    Add files via upload

---
+++ accounts/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Product, Category, Order, OrderItem, Customer, Page
+
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Customer)
+
+admin.site.register(Page)
